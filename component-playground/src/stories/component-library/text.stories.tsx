d1f4fd9f3fc3b523e3f9bdd188de5ccb3e4068aa
StephixOne <rares.capilnar@gmail.com>
2024-08-15 11:06:58 +0100

    Add a few more Text examples

--- component-playground/src/stories/component-library/text.stories.tsx
+++ component-playground/src/stories/component-library/text.stories.tsx
@@ -5,6 +5,13 @@ import { Text } from '../../../../components/component-library/Text/Text';
 const meta: Meta<typeof Text> = {
   title: "Component Library/Text",
   component: Text,
+  argTypes: {
+    variant: {
+      table: {
+        disable: true
+      }
+    }
+  },
   parameters: {
     layout: "centered",
   },
@@ -25,7 +32,7 @@ export const Headings: Story = {
 
 export const Body: Story = {
   render: () => (
-    <div className="flex flex-col gap-y-4">
+    <div className="flex flex-col gap-y-4 text-center items-center">
       <Text>Default Body Text</Text>
       <Text size="lg" weight="regular">Large Body Text</Text>
       <Text size="lg" weight="emphasised">Emphasised Large Body Text</Text>
@@ -34,6 +41,14 @@ export const Body: Story = {
       <Text size="sm" weight="regular">Small Body Text</Text>
       <Text size="sm" weight="emphasised">Emphasised Small Body Text</Text>
       <Text variant="label">A label</Text>
+      <Text variant="label" className="text-destructive">A label with an error</Text>
+      <div className="w-1/3 mt-3">
+        <Text variant="body" as="p" className="text-muted-foreground">
+          A longer paragraph, with some descriptive bits, maybe T&Cs?
+          <br/><br/>
+          A Terms and Conditions agreement acts as a legal contract between you and the user. It's where you maintain your rights to exclude users from your app in the event that they abuse your website/app, set out the rules for using your service and note other important details and disclaimers.
+        </Text>
+      </div>
     </div>
   )
 }
